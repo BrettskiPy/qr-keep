b4af8c7fcc0774f59de46dfb7ed5d4688b93c8bc
BrettskiPy <30988215+BrettskiPy@users.noreply.github.com>
2024-10-01 13:06:20 -0700

    Removed uneeded map model
    Removed uneeded map model

--- models/map_model.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-from datetime import datetime
-
-
-class TimeBoundParams(BaseModel):
-    start_time: Optional[datetime] = None
-    end_time: Optional[datetime] = None
